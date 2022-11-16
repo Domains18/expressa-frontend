5aed778c2d5bf10c8db9a87daa3e57df8cf5c17c
Domains18 <gibsonsgibson88@gmail.com>
2022-11-17 02:17:17 +0300

    comiit: app, image, header and about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
-
+import image from '../../Assets/img.jpeg'
+import './About.css'
 const About = () => {
   return (
-    <div id='About'>
+    <div id='about'>
         <div className="title-container">
-            <h2>Know More About us</h2>
+            <div>
+              <h2>Know More About Us</h2>
+            </div>
         </div>
+        <div className="about-us">
+            <img src={image} alt="" />
+            <div className="content-about">
+              <h1>what we do</h1>
+              <div className="gallery">
+                <article>
+                  <div className="title">
+                    <h2>Quality Writing</h2>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, facere.
+                  </div>
+                </article>
+                <article>
+                  <div className="title">
+                    <h2>Quality Writing</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, iusto?</p>
+                  </div>
+                </article>
+                <article>
+                  <div className="title">
+                    <h2>Quality Writing</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, veritatis?</p>
+                  </div>
+                </article>
+                <article>
+                  <div className="title">
+                    <h2>Quality Writing</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, veritatis?</p>
+                  </div>
+                </article>
+              </div>
+            </div>
+          </div>
     </div>
   )
 }
