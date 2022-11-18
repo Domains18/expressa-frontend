663cc7d2148db13082c6032977477eeb711f48f8
Domains18 <gibsonsgibson88@gmail.com>
2022-11-19 00:33:43 +0300

    added stories

---
+++ src/pages/Stories.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Stories = () => {
+    return (
+        <>
+            <div className="service-container">
+                <h1>Our Services</h1>
+            </div>
+            <div className="wrapper">
+                <div className="story-box">
+                    <div className="story-head">
+                        <h2>Wanna Write?</h2>
+                        <div className="story-content">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci distinctio dolorem odio rem ex et in consectetur architecto ipsam porro minima corrupti, voluptatum enim pariatur id sed consequatur quibusdam. Cupiditate aut corrupti consectetur in saepe neque sapiente ex ipsum aspernatur veniam, dignissimos fugit excepturi deleniti ipsam nemo, vero illum dolorum.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="story-box">
+                    <div className="story-head">
+                        <h2>Stories We tell</h2>
+                        <div className="story-content">
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci distinctio dolorem odio rem ex et in consectetur architecto ipsam porro minima corrupti, voluptatum enim pariatur id sed consequatur quibusdam. Cupiditate aut corrupti consectetur in saepe neque sapiente ex ipsum aspernatur veniam, dignissimos fugit excepturi deleniti ipsam nemo, vero illum dolorum.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Stories
