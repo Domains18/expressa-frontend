2fd581cead1dc771ec140a42a712c1c9d4369cfe
Domains18 <gibsonsgibson88@gmail.com>
2023-06-02 14:35:38 +0300

    linkup
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,14 @@
 //eslint-disable-next-line
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 import '../../scss/nav.components.scss'
 const Nav = () => {
   return (
     <nav className=' nav ' >
       <div className="our-logo">
-        <img src="https://www.docplanner.com/img/logo-default-group-en.svg?v=1" alt="logo" />
+        <Link to='/'>
+          <img src="https://www.docplanner.com/img/logo-default-group-en.svg?v=1" alt="logo" />
+        </Link>
       </div>
       <div className="nav-links">
         <span className='nav-link'>Dashboard</span>
