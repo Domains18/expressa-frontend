eb832c7ddfa68ed4aa3b3be09dbe2df1bb9a2e96
Domains18 <gibsonsgibson88@gmail.com>
2023-06-03 18:08:00 +0300

    error
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- src/Pages/Error/Error.js
+++ src/Pages/Error/Error.js
@@ -1,9 +1,23 @@
 //eslint-disable-next-line
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../scss/error.pages.scss';
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className="error-container">
+      <div className="error">
+        <div className="error-title">
+          <h1>ERROR <span>404</span></h1>
+        </div>
+        <div className="error-content">
+          <p>Requested page <span>NOT FOUND</span></p>
+          <Link>
+            Go back to <span className='home-link'>Dashboard</span>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
