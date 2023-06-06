e7ca04b148138e434056053fbdeca599ed6274bb
Domains18 <gibsonsgibson88@gmail.com>
2023-06-06 14:11:01 +0300

    footer
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 //eslint-disable-next-line
-import React from 'react'
-import Nav from './components/Nav/Nav'
-import Home from './Layouts/Home/Home'
+import React from 'react';
+import Nav from './components/Nav/Nav';
+import Home from './Layouts/Home/Home';
+import Footer from './Pages/Footer/Footer';
+
+
 const App = () => {
   return (
     <div>
       <Nav />
       <Home />
+      <Footer/>
     </div>
   )
 }
